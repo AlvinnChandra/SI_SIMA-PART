c52cb40d6b8a59b8c96f6cbe7027961227692acf
AlvinnChandra <alvinchandraunpar22@gmail.com>
2026-09-19 22:22:01 +0700

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const path = require("path");
 const cors = require("cors");
 const dbConnect = require("./config/dbConnect");
-
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const tokoRoutes = require("./routes/tokoRoutes");
 const itemRoutes = require("./routes/itemRoutes");
 const pesananRoutes = require("./routes/pesananRoutes");
 
-dotenv.config();
-
-// Connect to database
+// Connect to the database
 dbConnect();
 
 const app = express();
@@ -20,11 +17,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "../uploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -34,16 +27,16 @@ app.use("/api/items", itemRoutes);
 app.use("/api/pesanan", pesananRoutes);
 
 app.get("/api/hello", (req, res) => {
-  res.json({
-    message: "Hello dari Backend!"
-  });
+  res.json({ message: "Hello dari Backend!" });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-  res.status(400).json({
-    message: err.message
-  });
+  res.status(400).json({ message: err.message });
 });
 
-module.exports = app;
+// Start the server
+const PORT = process.env.PORT || 7002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
