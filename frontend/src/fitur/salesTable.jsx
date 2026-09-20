22f2652c572795943e9bcd39c4e7dcd616ec806b
AlvinnChandra <alvinchandraunpar22@gmail.com>
2026-09-20 21:30:26 +0700

    data sales vite

--- frontend/src/fitur/salesTable.jsx
+++ frontend/src/fitur/salesTable.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useLayoutEffect, useRef } from "react";
 import JSZip from "jszip";
 import "../css/salesTable.css";
 
-const API_BASE_URL = "http://localhost:7001/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:7001/api";
 const FILE_BASE_URL = "http://localhost:3000/uploads";
 
 // ============================================================
